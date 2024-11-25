fe590db48d4deaa33f06971ed24aa1bc0c992b76
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2024-11-25 12:32:29 +0900

    섹션4-2

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -6,10 +6,18 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+
 public class OrderApp {
+
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        //MemberService memberService = new MemberServiceImpl(null);
+        //OrderService orderService = new OrderServiceImpl(null,null);
+
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
