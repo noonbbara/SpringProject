686aa05f97fcc00e2a3fdb4216c88dd105574c49
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2025-01-10 00:32:39 +0900

    섹션4 완료

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,41 +1,38 @@
 package hello.core;
 
-<<<<<<< HEAD
+
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
-=======
-import hello.core.discount.FixDiscountPolicy;
->>>>>>> fe590db48d4deaa33f06971ed24aa1bc0c992b76
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
-<<<<<<< HEAD
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
-=======
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    }
-
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
->>>>>>> fe590db48d4deaa33f06971ed24aa1bc0c992b76
     }
 }
