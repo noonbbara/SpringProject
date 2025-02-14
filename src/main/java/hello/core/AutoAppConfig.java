f46a72fb7c09e99396bcf709ed23ee364abf81f1
noonbbara <ysys1226@naver.com>
2025-02-14 18:10:23 +0900

    섹션 7-1

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ConfigurationClassUtils;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan (
+        // 컴포넌트 스캔에서 뺄거를 필터링 (기존 예제 코드를 유지하기 위해, (실제론 안해도 됨))
+        excludeFilters = @ComponentScan.Filter (type = FilterType.ANNOTATION, classes = {Configuration.class})
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired // 자동으로 MemberRepository 타입에 맞는 거를 스프링이 찾아서 의존관계자동주입
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
